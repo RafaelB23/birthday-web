d1585e34f264a4c03237eb6c0ad9d90b93b60c6d
Ruben Barrientos <extern3189@bpd.com.do>
2025-03-28 02:38:13 -0700

    fix

--- src/app/components/box-photo/box-photo.component.ts
+++ src/app/components/box-photo/box-photo.component.ts
@@ -9,16 +9,16 @@ import { SparklesComponent } from '../sparkles/sparkles.component';
 })
 export class BoxPhotoComponent {
   items = [
-    {id:0, src:'imgs/MoonBirthday/1.ninaClinica.jpg', type:'img', alt:'La princesa', content:'Una etapa de la vida donde se comenzo a vivir muy intesno.'},
-    {id:1, src:'imgs/MoonBirthday/2.lenguitaTierna.jpg', type:'img', alt:'La niña tierna', content:'Esos ojos que te transportan a otro mundo.'},
-    {id:2, src:'imgs/MoonBirthday/3.boquitaChocolate.gif', type:'img', alt:'La mas comelona', content:'Gracias por dejar que tu niña interior se divirtiera este año'},
-    {id:3, src:'imgs/MoonBirthday/7.sonrisaLentes.jpg', type:'img', alt:'La sonrisa de mi día', content:'Sigue sonriendo que tu sonrisa sigue siendo la luz de mi vida.'},
-    {id:4, src:'imgs/MoonBirthday/8.lavaGirl.jpg', type:'img', alt:'Lava-girl', content:'Muchos ciclos de la vida, pero solo uno de amor.'},
-    {id:5, src:'imgs/MoonBirthday/91.ellaBonita.jpg', type:'img', alt:'La más sexy de todo el lugar', content:'Cada que sonreías, el mundo se detenia.'},
-    {id:6, src:'imgs/MoonBirthday/ninaBerrinche.jpeg', type:'img', alt:'Haciendo berrinche', content:'Nunca olvides que despues de la tormenta, siempre sale el sol.'},
-    {id:7, src:'imgs/MoonBirthday/v2.ninaConcentrada.gif', type:'img', alt:'Concentración maxima', content:'No dejes de ponerte objetivos y no te rindas hasta que tengas lo que quieres.'},
-    {id:8, src:'imgs/MoonBirthday/v6.camaritaFeliz.gif', type:'img', alt:'Muchos hermosos recuerdos', content:'Sigue tomando fotos, que cada una de ellas es un recuerdo que no se olvida.'},
-    {id:9, src:'imgs/MoonBirthday/v4.bizcosLengüita.gif', type:'img', alt:'Mi favorita ✨', content:'Sigue siendo feliz, que tu felicidad es la mia.'}
+    {id:0, src:'imgs/MoonBirthday/1.ninaClinica.jpg', alt:'La princesa', content:'Una etapa de la vida donde se comenzo a vivir muy intesno.'},
+    {id:1, src:'imgs/MoonBirthday/2.lenguitaTierna.jpg', alt:'La niña tierna', content:'Esos ojos que te transportan a otro mundo.'},
+    {id:2, src:'imgs/MoonBirthday/3.boquitaChocolate.gif', alt:'La mas comelona', content:'Gracias por dejar que tu niña interior se divirtiera este año'},
+    {id:3, src:'imgs/MoonBirthday/7.sonrisaLentes.jpg', alt:'La sonrisa de mi día', content:'Sigue sonriendo que tu sonrisa sigue siendo la luz de mi vida.'},
+    {id:4, src:'imgs/MoonBirthday/8.lavaGirl.jpg', alt:'Lava-girl', content:'Muchos ciclos de la vida, pero solo uno de amor.'},
+    {id:5, src:'imgs/MoonBirthday/91.ellaBonita.jpg', alt:'La más sexy de todo el lugar', content:'Cada que sonreías, el mundo se detenia.'},
+    {id:6, src:'imgs/MoonBirthday/ninaBerrinche.jpeg', alt:'Haciendo berrinche', content:'Nunca olvides que despues de la tormenta, siempre sale el sol.'},
+    {id:7, src:'imgs/MoonBirthday/v2.ninaConcentrada.gif', alt:'Concentración maxima', content:'No dejes de ponerte objetivos y no te rindas hasta que tengas lo que quieres.'},
+    {id:8, src:'imgs/MoonBirthday/v6.camaritaFeliz.gif', alt:'Muchos hermosos recuerdos', content:'Sigue tomando fotos, que cada una de ellas es un recuerdo que no se olvida.'},
+    {id:9, src:'imgs/MoonBirthday/v4.bizcosLengüita.gif', alt:'Mi favorita ✨', content:'Sigue siendo feliz, que tu felicidad es la mia.'}
   ]
 
   message = 
